03582c44069dc41dbba5523b108db407f3288c61
Vitaly Tulin <me@tulvit.net>
2024-04-15 20:53:07 +0300

    renaming js to ts

--- src/composables/getPosts.ts
+++ src/composables/getPosts.ts
@@ -3,23 +3,22 @@ import { ref } from 'vue'
 const getPosts = () => {
   const posts = ref([])
   const error = ref(null)
-  
+
   const load = async () => {
     try {
       let data = await fetch('https://jsonplaceholder.typicode.com/posts')
       if (!data.ok) {
-        throw Error('no data available');
+        throw Error('no data available')
       }
-      posts.value = await data.json();
+      posts.value = await data.json()
       if (!posts.value) {
         throw Error('error')
       }
-    }
-    catch (err) {
+    } catch (err: any) {
       error.value = err.message
     }
   }
-  
+
   return { posts, error, load }
 }
 
